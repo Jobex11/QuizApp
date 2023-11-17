8fb52a43e53195f17c9e316b2287bf876d0cbc80
jobex11 <joblawal33@gmail.com>
2023-11-17 23:38:15 +0100

    working on main page

--- quip-app/src/components/main.jsx
+++ quip-app/src/components/main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Nav from "./navbar";
 import "./main.css";
+import Content from "./content";
+import Sidebar from "./sidebar";
 
 const main = () => {
   return (
     <>
       <Nav />
+      <div className="container">
+        <Sidebar />
+        <Content />
+      </div>
     </>
   );
 };
