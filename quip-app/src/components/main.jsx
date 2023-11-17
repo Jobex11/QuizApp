989d31830bb5439a4627fd6bd68906af8b6f332b
jobex11 <joblawal33@gmail.com>
2023-11-17 23:09:24 +0100

    creating navbar

---
+++ quip-app/src/components/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./navbar";
+import "./main.css";
+
+const main = () => {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+};
+
+export default main;
---
+++ quip-app/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      {/*LOGO*/}
+
+      {/* user dashboard icon on the right*/}
+    </div>
+  );
+};
+
+export default Nav;
